d7d2802af7de1537e7cace890c47605449c78316
Mab <mabchhan@gmail.com>
2023-07-09 16:54:22 -0700

    edited errorMiddleware, and added productController

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,4 +1,4 @@
-const notfound = (req, res, next) => {
+const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404); // Set status to 404
   next(error); // Pass error to next middleware
@@ -20,4 +20,4 @@ const errorHandler = (err, req, res, next) => {
   });
 };
 
-export { notfound, errorHandler };
+export { notFound, errorHandler };
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,30 +1,12 @@
 import express from "express";
-//import products from "../data/products.js";
-import Product from "../models/productModel.js";
-import asyncHandler from "../middleware/asyncHandler.js";
-import e from "express";
+import {
+  getProduct,
+  getProductById,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      return res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+router.route("/").get(getProduct);
+router.route("/:id").get(getProductById);
 
 export default router;

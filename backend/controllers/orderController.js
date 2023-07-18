ff9e8a3c69197f54c89d97350970838b2f082bad
Mab <mabchhan@gmail.com>
2023-07-17 20:22:09 -0700

    order cart shipping

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,115 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Order from "../models/orderModel.js";
+
+// @desc   Create new order
+// @route   Post /api/orders
+// @access  Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+    // return;
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((item) => ({
+        ...item,
+        product: item._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
+});
+
+// @desc   Get logged in user orders
+// @route   Get /api/orders/myorders
+// @access  Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
+});
+
+// @desc   Get order by id
+// @route   Get /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+// @desc   Update order to paid
+// @route   Put /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    // Update order
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id, // from paypal
+      status: req.body.status, // from paypal
+      update_time: req.body.update_time, // from paypal
+      email_address: req.body.payer.email_address, // from paypal
+    };
+    const updatedOrder = await order.save();
+
+    // Return updated order
+    res.status(200).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+// @desc   Update order to delivered
+// @route   Put /api/orders/:id/deliver
+// @access  Private/admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  res.json("update order to delivered");
+});
+
+// @desc   Update an order
+// @route   Get /api/orders
+// @access  Private/admin
+const getOrders = asyncHandler(async (req, res) => {
+  res.json("update order to delivered");
+});
+
+export {
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getOrders,
+};
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,7 +15,14 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import { Provider } from "react-redux";
 import store from "./store.js";
+import PrivateRoute from "./components/PrivateRoute";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,6 +30,14 @@ const router = createBrowserRouter(
       <Route index="true" path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
+        <Route path="/placeorder" element={<PlaceOrderScreen />} />
+        <Route path="/order/:id" element={<OrderScreen />} />
+      </Route>
     </Route>
   )
 );
